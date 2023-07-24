5aaf807a8dcaaf583633b29f2e0062298f735604
josemichellpr <130313829+josemichellpr@users.noreply.github.com>
2023-07-24 10:26:43 -0600

    Delete HTML, CSS & JS/-ALGORITMOS Y PROGRAMACION- Verano 2023 UCIC directory

--- HTML, CSS & JS/-ALGORITMOS Y PROGRAMACION- Verano 2023 UCIC/NIVEL 4 - CONDICIONALES Y OPERADORES LOGICOS/numeroForzosoDeCeroACien.js
+++
@@ -1,8 +0,0 @@
-/*Programa que pide un número del 0 al 100 y comprueba si introduces un número correcto*/
-let numero=200
-if(0 <=numero &&  numero<= 100){
-  console.log("Muy bien")
-}
-else{
-  console.log("Vuelve a intentarlo")
-}
