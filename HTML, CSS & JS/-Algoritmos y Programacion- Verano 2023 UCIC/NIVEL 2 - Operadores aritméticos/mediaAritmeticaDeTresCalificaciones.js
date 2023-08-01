10bb09f6db04ab5ae4fa8889f567d138aa715c2e
josemichellpr <130313829+josemichellpr@users.noreply.github.com>
2023-07-31 18:56:26 -0600

    Delete HTML, CSS & JS/-Algoritmos y Programacion- Verano 2023 UCIC directory

--- HTML, CSS & JS/-Algoritmos y Programacion- Verano 2023 UCIC/NIVEL 2 - Operadores aritméticos/mediaAritmeticaDeTresCalificaciones.js
+++
@@ -1,9 +0,0 @@
-/*Hacer un programa que calcule la media aritmética de tres calificaciones*/
-
-let calificacion_1=7
-let calificacion_2=5
-let calificacion_3=10
-let numero_de_calificaciones=3
-let promedio=(calificacion_1+calificacion_2+calificacion_3)/numero_de_calificaciones
-let promedio_final=promedio.toFixed(2)
-console.log(promedio_final)
